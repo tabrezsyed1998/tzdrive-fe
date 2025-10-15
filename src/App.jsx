87512bf61e406aa2ffc4c9c1f0d99f6c73df9186
tabrezsyed1998 <tabrezsyed1998@gmail.com>
2025-10-15 22:24:10 +0530

    single level directory support completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,17 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import DirectoryView from "./DirectoryView";
 
-function App() {
-  const BASE_URL = "http://localhost:3000";
-  const [directoryItems, setDirectoryItems] = useState([]);
-  const [progress, setProgress] = useState(0);
-  const [newFilename, setNewFilename] = useState("");
 
-  async function getDirectoryItems() {
-    const response = await fetch(`${BASE_URL}/directory`);
-    const data = await response.json();
-    setDirectoryItems(data);
+
+const router = createBrowserRouter([
+  {
+    path: "/*",
+    element: <DirectoryView />
   }
-  useEffect(() => {
-    getDirectoryItems();
-  }, []);
+])
 
-  async function uploadFile(e) {
-    const file = e.target.files[0];
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", `${BASE_URL}/files/${file.name}`, true);
-    xhr.addEventListener("load", () => {
-      console.log(xhr.response);
-      getDirectoryItems();
-    });
-    xhr.upload.addEventListener("progress", (e) => {
-      const totalProgress = (e.loaded / e.total) * 100;
-      setProgress(totalProgress.toFixed(2));
-    });
-    xhr.send(file);
-  }
-
-  async function handleDelete(filename) {
-    const response = await fetch(`${BASE_URL}/files/${filename}`, {
-      method: "DELETE",
-    });
-    const data = await response.text();
-    console.log(data);
-    getDirectoryItems();
-  }
-
-  async function renameFile(oldFilename) {
-    console.log({ oldFilename, newFilename });
-    setNewFilename(oldFilename);
-  }
-
-  async function saveFilename(oldFilename) {
-    setNewFilename(oldFilename);
-    const response = await fetch(`${BASE_URL}/files/${oldFilename}`, {
-      method: "PATCH",
-      headers: {
-         "Content-Type" : "application/json"
-      },
-      body: JSON.stringify({newFilename}),
-
-    });
-    const data = await response.text();
-    console.log(data);
-    setNewFilename("");
-    getDirectoryItems();
-  }
-
-  return (
-    <>
-      <h1>My Files</h1>
-      <input type="file" onChange={uploadFile} />
-      <input
-        type="text"
-        onChange={(e) => setNewFilename(e.target.value)}
-        value={newFilename}
-      />
-      <p>Progress: {progress}%</p>
-      {directoryItems.map((item, i) => (
-        <div key={i}>
-          {item} <a href={`${BASE_URL}/files/${item}?action=open`}>Open</a>{" "}
-          <a href={`${BASE_URL}/files/${item}?action=download`}>Download</a>
-          <button onClick={() => renameFile(item)}>Rename</button>
-          <button onClick={() => saveFilename(item)}>Save</button>
-          <button
-            onClick={() => {
-              handleDelete(item);
-            }}
-          >
-            Delete
-          </button>
-          <br />
-        </div>
-      ))}
-    </>
-  );
+function App(){
+  return <RouterProvider router={router} />
 }
 
 export default App;
